85355952f1057a57362fd44e1d93891c64dc1f78
AgnesMuita <wamwithamuita@gmail.com>
2022-11-30 14:11:13 +0300

    assign serializable object to client instance

--- backend/graph_api.py
+++ backend/graph_api.py
@@ -21,18 +21,18 @@ def generate_access_token(app_id, scopes):
             access_token_cache = msal.SerializableTokenCache()
 
     # assign a SerializableTokenCache object to the client instance
-    client = msal.PublicClientApplication(client_id=app_id, token_cache=access_token_cache)
+    client_instance = msal.PublicClientApplication(client_id=app_id, token_cache=access_token_cache)
 
-    accounts = client.get_accounts()
+    accounts = client_instance.get_accounts()
     if accounts:
         # load session
-        token_response = client.acquire_token_silent(scopes, accounts[0])
+        token_response = client_instance.acquire_token_silent(scopes, accounts[0])
     else:
         # authenticate account
-        flow = client.initiate_device_flow(scopes=scopes)
+        flow = client_instance.initiate_device_flow(scopes=scopes)
         print('user_code: ' + flow['user_code'])
         webbrowser.open('https://microsoft.com/devicelogin')
-        token_response = client.acquire_token_by_device_flow(flow)
+        token_response = client_instance.acquire_token_by_device_flow(flow)
 
     with open('ms_graph_api_token.json', 'w') as _f:
         _f.write(access_token_cache.serialize())
