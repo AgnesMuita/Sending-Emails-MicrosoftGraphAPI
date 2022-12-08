e6609a6cd44e62f864aa13aa927d9ceb34c3d21f
AgnesMuita <wamwithamuita@gmail.com>
2022-12-08 18:32:46 +0300

    update web server configurations

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,33 @@
-import sys
-from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from index import send_email
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def add_headers (self):
+hostName = "localhost"
+serverPort = 8000
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
         self.send_header('Access-Control-Allow-Origin', '*')
-        SimpleHTTPRequestHandler.end_headers(self)
+        self.send_header('Access-Control-Allow-Methods', 'GET,POST, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>Hi, This is an example web server by Aggie.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+        print("send email")
         send_email()
 
-if __name__ == '__main__':
-    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 5000)
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
